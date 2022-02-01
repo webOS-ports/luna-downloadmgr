4d05af8b3f7cd4c00a5eb1aeb5c6cdb0913ebaed
Christophe Chapuis <chris.chapuis@gmail.com>
2022-02-01 20:08:52 +0000

    filesysStatusCheck: handle limits correctly
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- src/DownloadManager.h
+++ src/DownloadManager.h
@@ -203,7 +203,7 @@ public:
     static bool cbClearDownloadHistory(LSHandle * lshandle,LSMessage *msg,void * user_data);
     static bool cbFsStatusCheck(LSHandle * lshandle,LSMessage *msg,void * user_data);
 
-    void filesystemStatusCheck(const uint64_t& spaceFreeKB,const uint64_t& spaceTotalKB,bool * criticalAlertRaised = 0, bool * stopMarkReached = 0);
+    void filesystemStatusCheck(const uint64_t& spaceFreeKB,const uint64_t& spaceTotalKB,uint32_t *pctFullValue = 0, bool * stopMarkReached = 0);
 
     // upload specific
     static bool cbUpload(LSHandle* lshandle, LSMessage *message,void *user_data);
